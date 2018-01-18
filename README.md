47f165a32a8a418d18d450cf06b6bfa0e761b91a
Pelle <paccster@live.se>
2018-01-18 13:37:59 +0100

    Fixed functionality of dual subtitles and user input wordsize.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Projects intent is that you enter a couple of .srt-files into the program, and the program generates a set length of password consisting of a number of uncommon words found in the subtitles.
 
 Usage:
-$ sh subtitle-filter.sh <filename.srt> <amount of words>
+$ sh subtitle-filter.sh <filename.srt> <filename2.srt> <size per word> <amount of words>
--- subtitle-filter.sh
+++ subtitle-filter.sh
@@ -1,35 +1,54 @@
 #!/bin/sh
 
-SIZEOFPASS="$2"
-COUNTER=0
-
-if [ "$2" = "" ]; then
-	echo "*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*"
-	echo "Please enter wanted amount of words."
-	echo "*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*"
+if [ $# -ne 4 ]; then
+	echo "usage: $0 <file1.srt> <file2.srt> <word size> <amount of words>"
 	exit 1
-else
-	echo "*----------------------------------------*"
-	echo "$2 is the amount of words used in password"
-	echo ""
 fi
 
-echo "Filename entered: $1"
-echo ""
-cat $1 | grep -oE "( |$)[a-z]{5}( |$)" | sort -nr | uniq > results.txt
+check_fileexistance() #Check if file actually exists othewise stop
+{
+	if [ -f "$1" ]; then
+		echo "file "$1" exists"
+	else
+		echo "Please check your filename again."
+		exit 1
+	fi
+}
 
-echo -n "" > password.txt
-echo -n "" > prepassword.txt
+create_password() #Create password from the two entered files
+{
+	echo -n "" > prepass
+	cat $1 | grep -oE "( |$)[a-z]{$3}( |$)" | sort -nr > firstwordresult
+	cat $2 | grep -oE "( |$)[a-z]{$3}( |$)" | sort -nr > secondwordresult
+	COUNTER=0
+	while [ $COUNTER -lt "$4" ]; do
+		shuf -n 1 firstwordresult >> prepass
+		let COUNTER=COUNTER+1
+		shuf -n 1 secondwordresult >> prepass
+		let COUNTER=COUNTER+1
+	done
+	cat prepass | grep -oE [a-z]+ | tr -d '\n' > password.txt
+	rm firstwordresult
+	rm secondwordresult
+	rm prepass
+}
 
-while [ $COUNTER -lt $SIZEOFPASS ]; do
-	shuf -n 1 results.txt >> prepassword.txt
-	let COUNTER=COUNTER+1
-done
+validate_password() #Check if a passwordfile has been created
+{
+	if [ -f "password.txt" ]; then
+		echo "Password created!"
+		echo ""
+	else
+		echo "Password creation failed"
+		exit 1
+	fi
+}
 
-cat prepassword.txt | grep -oE [a-z]+ | tr -d '\n' > password.txt
-
-echo "Password created!"
-echo "*VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV*"
+#Execute functions to create password, if something is entered wrongly it will stop
+check_fileexistance $1
+check_fileexistance $2
+create_password $1 $2 $3 $4
+validate_password
 cat password.txt
 echo ""
-echo "*----------------------------------------*"
+echo ""
